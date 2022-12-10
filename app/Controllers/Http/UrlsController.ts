07b33203563ea12d40938d7cf74b5d82a8d84664
christopher sierra <christopher.sierra@fashionspark.com>
2022-12-09 21:11:20 -0300

    first ocmmit

--- app/Controllers/Http/UrlsController.ts
+++ app/Controllers/Http/UrlsController.ts
@@ -6,13 +6,13 @@ export default class UrlsController {
 
     public async store({ request, response }: HttpContextContract){
         
-        let uuid = nanoid(7)
+       /*  let uuid = nanoid(7)
         let ingreso = false
 
-        const url_corta = `${process.env.ORIGIN}/${uuid}`
+        const url_corta = `${process.env.ORIGIN}/${uuid}` */
 
         
-        while(!ingreso){
+      /*   while(!ingreso){
             try { 
                 const payload = await request.validate(CreateUrlValidator)
                 payload.uuid = uuid
@@ -23,10 +23,20 @@ export default class UrlsController {
             } catch(error) {
                 
                 uuid = nanoid(7)
-                // return response.send(error)
             
             }
-        }
+        } */
+
+
+        /* Intento 2:  */
+
+        const payload = await request.validate(CreateUrlValidator)
+        let uuid = nanoid(this.randomIntFromInterval(7, 16))
+        payload.uuid = uuid
+        const url_corta = `${process.env.ORIGIN}/${uuid}`
+        payload.url_corta = url_corta
+        const objeto = await Url.create(payload)
+        response.send(objeto)
 
 
     }
